1dc53307d98d468010d6e3686b943ec52a816772
Sir Qasim <m.qasim077@gmail.com>
2024-06-30 20:13:44 +0500

    c4

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1", "World2": "Pakistan zinda bad"}
+    return {"Hello": "World1", "World2": "Pakistan zinda bad 123"}
 
